759d62144aa5affbc03b13b7ab7193cb1827bdb5
kenkozheng <160150553@qq.com>
2014-11-06 15:55:39 +0800

    t

--- images/test/test/sugar/main.js
+++ images/test/test/sugar/main.js
@@ -53,7 +53,7 @@ cc.game.onStart = function(){
     cc.view.resizeWithBrowserSize(true);
     //load resources
     cc.LoaderScene.preload(g_resources, function () {
-        cc.director.runScene(new GameScene());
+        cc.director.runScene(new LevelListScene());
     }, this);
 };
 cc.game.run();
