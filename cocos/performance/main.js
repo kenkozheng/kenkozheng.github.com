216b2954e2455e8966067c014cf44573a33605e6
kenkozheng <160150553@qq.com>
2014-11-19 15:07:00 +0800

    performance test

---
+++ cocos/performance/main.js
@@ -0,0 +1,38 @@
+var PerformanceTest = cc.Scene.extend({
+	ctor:function(){
+		this._super();
+		
+		if("touches" in cc.sys.capabilities){
+            cc.eventManager.addListener({event: cc.EventListener.TOUCH_ONE_BY_ONE, onTouchBegan: this.addOne.bind(this)}, this);
+        }else{
+			cc.eventManager.addListener({event: cc.EventListener.MOUSE, onMouseDown:this.addOne.bind(this)}, this);
+        }
+	},
+	
+	addOne:function(){
+		for(var i = 0; i < 50; i++)
+		{
+			var sprite = new cc.Sprite("res/grossini.png");
+			var action = cc.rotateBy(1,100,100).repeatForever();
+			sprite.runAction(action);
+			this.addChild(sprite);
+			sprite.x = Math.random()*720;
+			sprite.y = Math.random()*1280;
+		}
+	}
+});
+
+cc.game.onStart = function(){
+    cc.view.adjustViewPort(true);
+    cc.view.setDesignResolutionSize(720, 1280, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.resizeWithBrowserSize(true);
+    //load resources
+    cc.LoaderScene.preload(["res/grossini.png"], function () {
+        cc.director.runScene(new PerformanceTest());
+    }, this);
+};
+cc.game.run();
+
+var trace = function() {
+    cc.log(Array.prototype.join.call(arguments, ", "));
+};
