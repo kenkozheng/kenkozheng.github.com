bb94f97e40719deb9ba6189eb59a8587d1814124
kenkozheng <kenkofox@qq.com>
2015-09-30 10:02:26 +0800

    add video

--- zego/js/product.js
+++ zego/js/product.js
@@ -2,11 +2,9 @@
  * Created by kenkozheng on 2015/9/29.
  */
 
-$(function () {
-    function onResize(){
-        $('.product-desc').height($('.product-desc').width()/2560*1440-30);
-    }
+function onResize(){
+    $('.product-desc').height($('.product-desc').width()/2560*1440-30);
+}
 
-    $(window).resize(onResize);
-    onResize();
-});
+$(window).resize(onResize);
+onResize();
